1b2ced5139a80875811df7f1b95770ca812a4b60
khushi-paneliya <paneliyakhushi777@gmail.com>
2026-01-31 08:10:44 +0530

    Add files via upload

---
+++ IO.py
@@ -0,0 +1,10 @@
+print("Hello World")
+print("i am khushi!!")
+
+#input using input()
+name = input("Enter your name:")
+print("hello",name) 
+
+num=int(input("Enter a number to square:"))
+square=num * num 
+print("the square is :",square)
