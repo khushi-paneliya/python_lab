973c7bdfddf910117731fa4af9078395afa81027
khushi-paneliya <paneliyakhushi777@gmail.com>
2026-02-24 22:03:30 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,20 @@
+i=1
+while i<=5:
+    print(i)
+    i=i+1
+i=1
+while i<=10:
+    print(i)
+    i=i+1
+n=int(input("Enter n:"))
+i=1
+s=0
+while i<=n:
+    s=s+i
+    i=i+1
+print("sum =",s)    
+num = int (input("Eneter number:"))
+i=1
+while i<=10:
+    print (num,"X",i,"=",num*i)
+    i=i+1
