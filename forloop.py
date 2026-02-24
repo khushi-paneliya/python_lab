6fa25811de8368a52b2cfd9b627488bda1efa68b
khushi-paneliya <paneliyakhushi777@gmail.com>
2026-02-24 08:20:07 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,25 @@
+for i in range(1,6):
+    print(i)
+for  i in  range(3):
+    print("Hello Khushi")
+for i in range(1,11):
+    print(i)
+for i in range (1,21):
+    if i%2==0:
+        print(i)
+for i in range (1,16):
+    if i%2==1:
+        print(i)
+for i in range (1,11):
+    print("5 *",i,"=",5*i)
+name="Atmiya"
+for letter in name:
+    print(letter)
+total=0
+for i in range (1,6):
+    total = total + i
+    print("Sum of total",total)
+numbers=[10,20,30,40]
+for n in numbers:
+    print(n)
+    
