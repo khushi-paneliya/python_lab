4f2d0b967362a70b0d9b9e18332612d95962d3c2
khushi-paneliya <paneliyakhushi777@gmail.com>
2026-03-07 09:05:36 +0530

    Add files via upload

---
+++ indexingArrays.py
@@ -0,0 +1,27 @@
+"""from array import array
+arr = array('i',[10,20,30,40,50])
+print(arr[0])
+print(arr[2])"""
+"""
+from array import array
+arr = array('i',[10,20,30,40,50])
+print(arr[-1])#last element
+print(arr[-2])#second
+print(arr[-5])#first"""
+"""
+from array import array
+arr = array('i',[10,20,30,40,50])
+arr[2] = 35
+print(arr)"""
+"""
+from array import array
+arr=array('i',[10,20,30])
+print(arr[5])
+"""
+#Slicing in array
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[1:4])#1 to 3 
+print(arr[:3])#start to index 2
+print(arr[2:])#2 to end
+print(arr[:])# entire array
