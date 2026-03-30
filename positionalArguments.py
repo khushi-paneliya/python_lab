76e0362e5472549850be31b9f8645f6b3f100b06
khushi-paneliya <paneliyakhushi777@gmail.com>
2026-03-30 22:41:39 +0530

    Add files via upload

---
+++ positionalArguments.py
@@ -0,0 +1,59 @@
+"""# basic
+def add(a,b):
+    print("a=",a)
+    print("b=",b)
+    return a+b
+result = add(2,5)
+print("sum=",result)
+
+#student info
+def student_info(name,roll,marks):
+    print("Name:",name)
+    print("Roll no.:",roll)
+    print("Marks :",marks)
+
+student_info("khushi",45,87)
+"""
+#simple interst
+
+"""def simple_interst(p,r,n):
+    si=(p*r*n)/100
+    print("simple Interest :",si)
+simple_interst(100000,2,8)
+simple_interst(50000,7,5)"""
+
+# area of circle 
+"""
+def ar_circle(r):
+    ar_circle=3.14 * r*r
+    print("Area of circle :",ar_circle)
+ar_circle(1.5)
+ar_circle(4)"""
+
+#check number positive negative or zero
+"""def check_value(no):
+    if (no>0):
+        print("positive")
+    elif(no<0):
+        print("negative")
+    else:
+        print("zero")
+check_value(0)
+check_value(-10)
+check_value(6)"""
+
+#odd or even
+"""def odd_even(no):
+    if(no %2 == 0):
+        print(f"value {no} is even")
+    else:
+        print(f"value {no} is odd")
+odd_even(45)
+odd_even(20)"""
+
+#arithmetic operation substraction multipilication and division
+def addition(a,b):
+    add = a + b
+    print("Addition of two value ",add)
+addition(50,58)
+addition(20,40)
